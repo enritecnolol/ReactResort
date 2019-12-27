19a9f1b48faba952aa0c76971e1a4b35e5f20a60
Tomas Crespo <enritecnolol@gmail.com>
2019-12-27 09:45:13 -0400

    building banner for home, rooms and 40

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Hero from "../components/Hero";
-
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 const Rooms = () => {
     return (
-        <Hero hero="roomsHero"/>
+        <Hero hero="roomsHero">
+            <Banner title="Our rooms">
+                <Link to="/" className="btn-primary">
+                    return Home
+                </Link>
+            </Banner>
+        </Hero>
     )
 };
 
